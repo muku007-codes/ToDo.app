9272dd4a7b763220c8d9e4a480e39e9c1f341c09
omuksa007@gmail.com <omuksa007@gmail.co>
2022-03-19 14:14:29 +0530

    Include localhost

--- script.js
+++ script.js
@@ -1,22 +1,101 @@
 let addtodobutton = document.getElementById("addtodos");
-let todocontainer = document.getElementById("todocontainer");
-let inputfield = document.getElementById("inputfiled");
+let input = document.getElementById("input");
+let todosUL = document.getElementById("todos");
 
-addtodobutton.addEventListener('click',()=>{
-    var paragraph = document.createElement('p');
-    paragraph.innerHTML = inputfield.value;
-    todocontainer.appendChild(paragraph);
-    inputfield.value = "";
+const todos = JSON.parse(localStorage.getItem("todos"));
 
-    paragraph.addEventListener('click',()=>{
-        paragraph.style.textDecoration = "line-through";
-        paragraph.style.background = "#FFAFAF";
-    })
+if(todos)
+{
+    todos.forEach((todo) => {
+        addTodo(todo);
+    });
+}
 
-    paragraph.addEventListener('dblclick',()=>{
-        todocontainer.removeChild(paragraph);
-    })
-})
+addtodobutton.addEventListener('click',(e) => {
+    e.preventDefault();
+    addTodo();
+});
+
+function addTodo(todo) {
+    let todoText = input.value;
+
+    if (todo) {
+        todoText = todo.text;
+    }
+
+    if (todoText) {
+        const todoEl = document.createElement("p");
+        if (todo && todo.completed) {
+            todoEl.classList.add("completed");
+        }
+
+        todoEl.innerText = todoText;
+
+        todoEl.addEventListener("click", () => {
+            todoEl.classList.toggle("completed");
+
+            updateLS();
+        });
+
+        todoEl.addEventListener("dblclick", (e) => {
+            e.preventDefault();
+
+            todoEl.remove();
+
+            updateLS();
+        });
+
+        todosUL.appendChild(todoEl);
+
+        input.value = "";
+
+        updateLS();
+    }
+}
+
+function updateLS() {
+    const todosEl = document.querySelectorAll("p");
+
+    const todos = [];
+
+    todosEl.forEach((todoEl) => {
+        todos.push({
+            text: todoEl.innerText,
+            completed: todoEl.classList.contains("completed"),
+        });
+    });
+
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+
+//old code
+
+// addtodobutton.addEventListener('click',()=>{
+    //     var paragraph = document.createElement('p');
+//     paragraph.innerHTML = inputfield.value;
+
+//     let str = inputfield.value;
+//     localStorage.setItem(str,str);
+
+//     todocontainer.appendChild(paragraph);
+//     inputfield.value = "";
+//forEach() to loop an array and save to localStorage and whenever page is loaded all items automaticaly created
+
+    // paragraph.addEventListener('click',()=>{  
+    //     paragraph.style.textDecoration = "line-through";
+    //     paragraph.style.background = "#FFAFAF";
+    // })
+
+    // paragraph.addEventListener('dblclick',()=>{
+    //     let removestr = paragraph.innerText;
+    //     localStorage.removeItem(removestr);
+    //     todocontainer.removeChild(paragraph);
+    // })
+
+// })
+
+//for dark/light mode
 
 function change()
 {
